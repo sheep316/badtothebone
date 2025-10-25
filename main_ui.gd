c221eb3d4546af0867a2a23dd7cf3f241c715324
tanzettt <taotan429@gmail.com>
2025-10-25 15:21:47 +0800

    button code

--- main_ui.gd
+++ main_ui.gd
@@ -1,6 +1,22 @@
 extends Control
 
-
+var button_pressed = false
 
 func _on_fight_pressed() -> void:
+	button_pressed = true
 	print("pwned")
+
+
+func _on_act_pressed() -> void:
+	button_pressed = true
+	print("acted")
+
+
+func _on_item_pressed() -> void:
+	button_pressed = true
+	print("used item")
+
+
+func _on_mercy_pressed() -> void:
+	button_pressed = true
+	print("surrendered")
